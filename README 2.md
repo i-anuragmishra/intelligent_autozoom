66093c1e338ad78eb2513790da361ba7babb9190
anurag <anuragmishra.ofc@gmail.com>
2021-03-13 19:11:38 +0530

    Added the app

---
+++ README 2.md
@@ -0,0 +1,2 @@
+# intelligent_autozoom-
+Intelligent Autozoom app for GSoC 21
---
+++ android/app/src/main/kotlin/com/example/intelligent_autozoom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.intelligent_autozoom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
